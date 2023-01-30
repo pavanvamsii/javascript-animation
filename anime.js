f92d2a17f1924a86003f57ac61b4b7f9bd8f927d
pavanvamsii <singampallipavanvamsi@gmail.com>
2023-01-30 16:34:01 +0530

    animation

---
+++ anime.js
@@ -0,0 +1,41 @@
+function one(){
+    document.querySelector(".slider").src = "./img/th (3).jpg";
+}
+function two(){
+    document.querySelector(".slider").src = "./img/man2.jpg";
+}
+function three(){
+    document.querySelector(".slider").src = "./img/man3.png";
+}
+function four(){
+    document.querySelector(".slider").src = "./img/man4.png";
+}
+function five(){
+    document.querySelector(".slider").src = "./img/man5.png";
+}
+function six(){
+    document.querySelector(".slider").src = "./img/acket.jpg";
+}
+function seven(){
+    document.querySelector(".slider").src = "./img/box.jpg";
+}
+function eight(){
+    document.querySelector(".slider").src = "./img/buggi2.png";
+}
+function nine(){
+    document.querySelector(".slider").src = "./img/Screenshot1 2022-07-29 141408.png";
+}
+function ten(){
+    document.querySelector(".slider").src = "./img/parashoot.png";
+}
+setInterval(one,1000)
+setInterval(two,2000)
+setInterval(three,3000)
+setInterval(four,4000)
+setInterval(five,5000)
+setInterval(six,6000)
+setInterval(seven,7000)
+setInterval(eight,8000)
+setInterval(nine,9000)
+setInterval(ten,10000)
+
